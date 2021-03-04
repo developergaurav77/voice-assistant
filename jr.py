eb8b6090e8c855f996b77cce46a088fccdfd0267
developergaurav77 <developer.gaurav77@gmail.com>
2021-03-04 21:53:49 +0545

    time function created

--- jr.py
+++ jr.py
@@ -1,4 +1,5 @@
 import pyttsx3  #importing this module to convert text to speech
+import datetime #importing this module to use date and time
 
 engine = pyttsx3.init() # object creation
 
@@ -7,5 +8,16 @@ engine.getProperty('rate')   # getting details of current speaking rate
 voiceRate = 170
 engine.setProperty('rate', voiceRate)     # setting up new voice rate
 
-engine.say("I will speak this text")
-engine.runAndWait()
+
+
+def speak(audio): #this function to used to speak
+    engine.say(audio)
+    engine.runAndWait()
+    
+def time():  #this function is used to calculate current time
+    time = datetime.datetime.now().strftime("%I:%M:%S")
+    print(time)
+    
+
+speak('i m good')
+time()
